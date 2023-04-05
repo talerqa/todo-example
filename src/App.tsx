9aa209865b5fecbc0f32232f1812ad837a945807
talerqa <hitri3003@gmail.com>
2023-04-05 19:25:56 +0300

    feat: add button which delete all tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,244 +1,136 @@
 import React, {useState} from 'react';
 import './App.css';
-import Tasks from './Task';
+import {Todolist} from './Todolist';
 
-type taskType = {
-  taskId: number
-  title: string
-  isDone: boolean
-}
+export type FilterValuesType = 'all' | 'active' | 'completed' ;
 
-export type dataType = Array<taskType>
-
-type StatusTasksType = 'All' | 'Active' | 'Completed'
+//Hi guys!
+//1. Let's create a 'DELETE ALL TASKS' button, and place it above the filter buttons
+//Clicking the button removes all tasks
+//2. Let's create a fourth filter button-if you click it, the first three tasks will be displayed
+//3. Relocate everything associated with  filters to the Todolist.tsx component. Make it work
+//
+// let [filter, setFilter] = useState<FilterValuesType>("all");
+//
+// let tasksForTodolist = tasks;
+//
+// if (filter === "active") {
+//     tasksForTodolist = tasks.filter(t => t.isDone === false);
+// }
+// if (filter === "completed") {
+//     tasksForTodolist = tasks.filter(t => t.isDone === true);
+// }
+//
+// function changeFilter(value: FilterValuesType) {
+//     setFilter(value);
+// }
 
 function App() {
-  const data: dataType = [
-    {taskId: 1, title: 'Homework', isDone: true},
-    {taskId: 2, title: 'Pushups', isDone: false},
-    {taskId: 3, title: 'Reading', isDone: true},
-    {taskId: 4, title: 'Breathe practice', isDone: false},
-    {taskId: 5, title: 'Jogging', isDone: true},
-  ]
 
-  const [tasks, setTasks] = useState(data);
-  const removeTask = (id: number) => {
-    setTasks(tasks.filter(task => task.taskId !== id))
+  let [tasks, setTasks] = useState([
+    {id: 1, title: 'HTML&CSS', isDone: true},
+    {id: 2, title: 'JS', isDone: true},
+    {id: 3, title: 'ReactJS', isDone: false},
+    {id: 4, title: 'Rest API', isDone: false},
+    {id: 5, title: 'GraphQL', isDone: false},
+  ]);
+
+  function removeTask(id: number) {
+    let filteredTasks = tasks.filter(t => t.id != id);
+    setTasks(filteredTasks);
   }
 
-  const [filter, setFilter] = useState ('All');
-
-  const statusTasks = (filter: any) => {
-    setFilter(filter)
+  function deleteAllTasks() {
+      setTasks([])
   }
 
-  const taskWhatIWantGet = (tasks: any, filter: any) => {
-    if (filter === 'Active') {
-      return tasks.filter((t: any) => t.isDone !== true)
-    } else if (filter === 'Completed') {
-      return tasks.filter((t: any) => t.isDone !== false)
-    } else {
-      return tasks
-    }
+  function returnDeleteTasks() {
+    setTasks(tasks)
   }
 
-  const taskWhatIWantSee = taskWhatIWantGet(tasks, filter)
+  let [filter, setFilter] = useState<FilterValuesType>('all');
+
+  let tasksForTodolist = tasks;
+
+  if (filter === 'active') {
+    tasksForTodolist = tasks.filter(t => t.isDone === false);
+  }
+
+  function changeFilter(value: FilterValuesType) {
+    setFilter(value);
+  }
 
   return (
+
     <div className="App">
-      <Tasks
-        state={taskWhatIWantSee}
-        removeTask={removeTask}
-        statusTasks={statusTasks}
-      />
+      <Todolist title="What to learn"
+                tasks={tasksForTodolist}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+                returnDeleteTasks={returnDeleteTasks}
+                deleteAllTasks={deleteAllTasks}/>
     </div>
   );
-
 }
 
 export default App;
 
 
-//--------------------------------------------------------------
-// import React from 'react';
-// import './App.css';
-// import
-//{
-// Tasks
-// }
-//from "./Tasks";
-//
-// export type DataType =
-//{
+//-------------------------------------------------------------------------
 
-//     title: string
-//     tasks: Array<TasksType>
-//     students: Array<string>
-// }
-// export type TasksType = {
-//     taskId: number
-//     title: string
-//     isDone: boolean
-// }
+// import React, {useState} from 'react';
+// import './App.css';
+// import {Todolist} from './Todolist';
 //
+//
+// export type FilterValuesType = "all" | "active" | "completed" | "three";
 //
 // function App() {
-//     const data1= {
-//         title: "What to do",
-//         tasks: [
-//             {taskId: 1, title: "HTML&CSS2", isDone: true},
-//             {taskId: 2, title: "JS2", isDone: true}
-//         ],
-//         students: [
-//             'Jago Wormald1',
-//             'Saul Milne2',
-//             'Aariz Hester3',
-//             'Dion Reeve4',
-//             'Anisa Ortega5',
-//             'Blade Cisneros6',
-//             'Malaikah Phelps7',
-//             'Zeeshan Gallagher8',
-//             'Isobella Vo9',
-//             'Rizwan Mathis10',
-//             'Menaal Leach11',
-//             'Kian Walton12',
-//             'Orion Lamb13',
-//             'Faizah Huynh14',
-//             'Crystal Vaughan15',
-//             'Vivien Hickman16',
-//             'Stuart Lu17',
-//             'Karol Davison18',
-//             'Dario Burns19',
-//             'Chloe Rich20',
-//             'Martyna Felix',
-//             'Nida Glass',
-//             'Maeve Miles',
-//             'Hasnain Puckett',
-//             'Ayman Cano',
-//             'Safwan Perry',
-//             'Fox Kelly',
-//             'Louise Barlow',
-//             'Malaki Mcgill',
-//             'Leanna Cline',
-//             'Willard Hodge',
-//             'Amelia Dorsey',
-//             'Kiah Porter',
-//             'Jeanne Daly',
-//             'Mohsin Armstrong',
-//             'Laurie Rangel',
-//             'Princess Tierney',
-//             'Kasim Kendall',
-//             'Darryl Cope',
-//             'Elysha Ray',
-//             'Liyana Harris',
-//             'Kashif Blackburn',
-//             'Atif Zimmerman',
-//             'Sila Hartley',
-//             'Ralphie Hebert',
-//         ]
+//
+//     let [tasks, setTasks] = useState([
+//         {id: 1, title: "HTML&CSS", isDone: true},
+//         {id: 2, title: "JS", isDone: true},
+//         {id: 3, title: "ReactJS", isDone: false},
+//         {id: 4, title: "Rest API", isDone: false},
+//         {id: 5, title: "GraphQL", isDone: false},
+//     ]);
+//
+//     const deleteAllTasks = () => {
+//         setTasks([])
 //     }
-//     const data2 = {
-//         title: "What to learn",
-//         tasks: [
-//             {taskId: 1, title: "HTML&CSS", isDone: true},
-//             {taskId: 2, title: "JS", isDone: true}
-//         ],
-//         students: [
-//             'Rick Kane',
-//             'Finnlay Bentley',
-//             'Samia North',
-//             'Isaac Morton',
-//             'Lily-Ann Clifford',
-//             'Thalia Park',
-//             'Sapphire Cruz',
-//             'Cieran Vazquez',
-//             'Anya Estes',
-//             'Dominika Field',
-//             'Rosanna Chung',
-//             'Safiyah Davey',
-//             'Ryley Beasley',
-//             'Kalvin Trejo',
-//             'Evie-Mae Farrell',
-//             'Juliet Valencia',
-//             'Astrid Austin',
-//             'Lyle Montgomery',
-//             'Nisha Mora',
-//             'Kylie Callaghan',
-//             'Star Wilks',
-//             'Marissa Colley',
-//             'Asa Fuller',
-//             'Leigh Kemp',
-//             'Avleen Dawson',
-//             'Sammy Bonilla',
-//             'Acacia Becker',
-//             'Coral Shepherd',
-//             'Melina Molina',
-//             'Kiran Bailey',
-//             'Clara Escobar',
-//             'Alexandru Horn',
-//             'Brandon-Lee Mercado',
-//             'Elouise Weston',
-//             'King Long',
-//             'Kerri Searle',
-//             'Kanye Hamer',
-//             'Elwood Benitez',
-//             'Mikail Whitaker',
-//             'Bobby Hardy',
-//             'Talha Ferry',
-//             'Priscilla Landry',
-//             'Olivia-Grace Cain',
-//             'Kiaan Wallace',
-//             'Wesley Padilla90',
-//             'Ella-Grace Wooten91',
-//             'Kaif Molloy92',
-//             'Kamal Broadhurst93',
-//             'Bianca Ferrell94',
-//             'Micheal Talbot95',
-//         ]
+//
+//     function removeTask(id: number) {
+//         let filteredTasks = tasks.filter(t => t.id != id);
+//         setTasks(filteredTasks);
 //     }
+//
+//     // let [filter, setFilter] = useState<FilterValuesType>("all");
+//     //
+//     // let tasksForTodolist = tasks;
+//     //
+//     // if (filter === "active") {
+//     //     tasksForTodolist = tasks.filter(t => t.isDone === false);
+//     // }
+//     // if (filter === "completed") {
+//     //     tasksForTodolist = tasks.filter(t => t.isDone === true);
+//     // }
+//     //
+//     // function changeFilter(value: FilterValuesType) {
+//     //     setFilter(value);
+//     // }
 //
 //     return (
 //         <div className="App">
-//             <Tasks data={data1}/>
-//             <Tasks data={data2}/>
+//             <Todolist
+//                 title="What to learn"
+//                 tasks={tasks}
+//                 removeTask={removeTask}
+//                 //changeFilter={changeFilter}
+//                 deleteAllTasks={deleteAllTasks}
+//
+//             />
 //         </div>
 //     );
 // }
 //
 // export default App;
-
-// import React from 'react';
-// import {DataType} from "./App";
-//
-// type  TasksPropsType = {
-//     data: DataType
-// }
-//
-// export const Tasks = (props: TasksPropsType) => {
-//     return (
-//         <div>
-//             <h1>{props.data.title}</h1>
-//             <ul>
-//                 {props.data.tasks.map(el => {
-//                     return (
-//                         <li>
-//                             <span>{el.taskId}</span>
-//                             <span>{el.title}</span>
-//                             <span>{el.isDone}</span>
-//                         </li>
-//                     )
-//                 })}
-//             </ul>
-//
-//             <ul>
-//                 {props.data.students.map(el => {
-//                     return (
-//                         <li>{el}</li>
-//                     )
-//                 })}
-//             </ul>
-//         </div>
-//     );
-// };
-
-//--------------------------------------------------------------
